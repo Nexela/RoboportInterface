7b481141adaa38ff20b12ffc3da7a2455ebc7b7d
Nicholas Dunn <nexela@gmail.com>
2022-03-11 21:15:29 -0500

    Mutate fix

--- data-updates.lua
+++ data-updates.lua
@@ -1,7 +1,8 @@
 local Recipe = require('__stdlib__/stdlib/data/recipe')
 
 if mods['boblibrary'] then
-    if settings.get_startup('bobmods-logistics-disableroboports') then
+    local key = 'bobmods-logistics-disableroboports'
+    if settings["startup"][key] and settings["startup"][key].value then
         Recipe('roboport-interface'):replace_ingredient('roboport', 'bob-logistic-zone-expander')
     end
 end
